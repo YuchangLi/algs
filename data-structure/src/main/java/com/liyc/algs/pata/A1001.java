9fb16d0f30bcd71ec596202f9a1281101eeda1fe
li_yc <648821884@qq.com>
2019-10-17 01:23:43 +0800

    add 1002

--- data-structure/src/main/java/com/liyc/algs/pata/A1001.java
+++ data-structure/src/main/java/com/liyc/algs/pata/A1001.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Hello world!
+ * 1,000
+ * 0123
+ * 3210
+ * 1,000,000
+ * 0123456
+ * 6543210
+ * 规律：i对于的(length-1-i) % 3 == 0 且大于0
  */
 public class A1001 {
     public static void main(String[] args) throws IOException {
--- data-structure/src/main/java/com/liyc/algs/pata/A1002.java
+++ data-structure/src/main/java/com/liyc/algs/pata/A1002.java
@@ -1,37 +1,40 @@
 package com.liyc.algs.pata;
 
-import java.util.Comparator;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 /**
- * Hello world!
+ * array[指数] = 系数
+ * 1. 系数相加为0时总数减1
+ * 2. 系数可能为负数，固最后不能as[i]>0
  */
 public class A1002 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int k1 = in.nextInt();
-        Map<Integer, Double> map = new TreeMap<>(Comparator.comparingDouble(ko -> -ko));
-
-        for (int i = 0; i < k1; i++) {
-            Integer key = in.nextInt();
-            double value = in.nextDouble();
-            map.put(key, value);
-        }
-        int k2 = in.nextInt();
-        for (int i = 0; i < k2; i++) {
-            Integer key = in.nextInt();
-            double value = in.nextDouble();
-            if(map.containsKey(key)){
-                map.put(key, map.get(key)+value);
-            }else{
-                map.put(key, value);
-            }
-        }
-        System.out.print(map.size());
-        map.forEach((k, v)->{
-            System.out.print(" "+k+" "+String.format("%.1f", v));
-        });
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int k1 = in.nextInt();
+		double[] as = new double[1005];
+		int count = k1;
+		for (int i = 0; i < k1; i++) {
+			Integer key = in.nextInt();
+			double value = in.nextDouble();
+			as[key] = value;
+		}
+		int k2 = in.nextInt();
+		for (int i = 0; i < k2; i++) {
+			Integer key = in.nextInt();
+			double value = in.nextDouble();
+			if (as[key] == 0) {
+				count++;
+			}
+			as[key] = as[key] + value;
+			if (as[key] == 0) {
+				count--;
+			}
+		}
+		System.out.print(count);
+		for (int i = as.length - 1; i >= 0; i--) {
+			if (as[i] != 0) {
+				System.out.print(" " + i + " " + String.format("%.1f", as[i]));
+			}
+		}
+	}
 }
