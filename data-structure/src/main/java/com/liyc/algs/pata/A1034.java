820f3ffae818b8e7b715969da4fd6cc3ef2835d7
liyuchang <liyuchang@ruubypay.com>
2019-10-18 15:33:14 +0800

    1034

--- data-structure/src/main/java/com/liyc/algs/pata/A1034.java
+++ data-structure/src/main/java/com/liyc/algs/pata/A1034.java
@@ -1,104 +1,118 @@
 package com.liyc.algs.pata;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * array[指数] = 系数
- * 1. 系数相加为0时总数减1
- * 2. 系数可能为负数，固最后不能as[i]>0
+ * 构建Map<index, name>, 构建g[name1][name2]
  */
 public class A1034 {
 	private static int[][] g;
 	private static int[] v;
+	private static int[] w;
 	private static int threthold;
 	private static int maxpersons = 90;
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String line1 = in.nextLine();
-
+	public static void main(String[] args) throws IOException {
+		long s = System.currentTimeMillis();
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		String line1 = bf.readLine();
 		String[] line1as = line1.split(" ");
 		int totalcalls = Integer.valueOf(line1as[0]);
 		threthold = Integer.valueOf(line1as[1]);
-
-//		maxpersons = totalcalls * 2;
-
+		maxpersons = totalcalls * 2;
+		int index = 0;
+		Map<Integer, String> persons = new HashMap<>(maxpersons);
 		g = new int[maxpersons][maxpersons];
 		v = new int[maxpersons];
-		int[] w = new int[maxpersons];
-		Map<Integer, List<Integer>> subg = new HashMap<>();
-		Map<Integer, Integer> maxtime = new HashMap<>();
+		w = new int[maxpersons];
+
 
 		for (int k = 0; k < totalcalls; k++) {
-			String[] lines = in.nextLine().split(" ");
-//			System.out.println(Arrays.asList(lines));
-			int i = lines[0].charAt(0);
-			int j = lines[1].charAt(0);
-			int time = Integer.valueOf(lines[2]);
-			if(g[j][i] > 0){
-				g[j][i] = g[j][i] + time;
-			}else {
-				g[i][j] = time;
+			String[] lines = bf.readLine().split(" ");
+			String name1 = lines[0];
+			String name2 = lines[1];
+			int i = 0;
+			if(!persons.containsValue(name1)){
+				i = index++;
+				persons.put(i, name1);
+			} else{
+				i = getKeyFromValue(persons, name1);
 			}
+			int j = 0;
+			if(!persons.containsValue(name2)){
+				j = index++;
+				persons.put(j, name2);
+			} else{
+				j = getKeyFromValue(persons, name2);
+			}
+			int time = Integer.valueOf(lines[2]);
+			g[i][j] += time;
+			g[j][i] += time;
 			w[j] = w[j] + time;
 			w[i] = w[i] + time;
 		}
-
-		for (int i = 0; i < maxpersons; i++) {
-			if( w[i] == 0 || v[i] > 0){
+		bf.close();
+		Set<Set<Integer>> subg = new HashSet<>();
+		for (int i = 0; i < persons.size(); i++) {
+			if( v[i] > 0){
 				continue;
 			}
-//			System.out.println("i = "+i+", "+String.valueOf((char)i));
-			List<Integer> list = new ArrayList<>(maxpersons);
-			list.add(0);
-//			list.add(i);
-			subg.put(i, list);
-			dfs(i, list);
+			Set<Integer> set = new HashSet<>(maxpersons);
+			subg.add(set);
+			dfs(i, set);
 		}
-		AtomicInteger count = new AtomicInteger();
-		Map<Integer, Integer> ps = new TreeMap<>();
-		subg.forEach((k, list)->{
-//			System.out.print((char)k.intValue()+" : ");
-//			for (int i = 1; i < list.size(); i++) {
-//				System.out.print((char)list.get(i).intValue());
-//			}
-//			System.out.print(" size = "+list.size()+" maxtime="+list.get(0));
-//			System.out.println();
-			if(list.size() <= 3){
-				return;
+		int count = 0;
+		Map<Integer, Integer> ps = new TreeMap<>(Comparator.comparing(persons::get));
+		for (Set<Integer> set : subg){
+			if(set.size() <= 2){
+				continue;
 			}
-			if(list.get(0) <= threthold ){
-				return;
-			}
-			count.getAndIncrement();
-			int max = list.get(1);
-			for (int i = 2; i < list.size(); i++) {
-				if(w[list.get(i)] > w[max]){
-					max = list.get(i);
+			int max = -1;
+			int sum = 0;
+			for (int i : set) {
+				int iw = w[i];
+				sum += iw;
+				if (max == -1 || iw > w[max]) {
+					max = i;
 				}
 			}
-			ps.put(max, list.size()-1);
-		});
+			if( sum /2 <= threthold) {
+				continue;
+			}
+			count++;
+			ps.put(max, set.size());
+		}
 		System.out.println(count);
-		if(count.intValue()==0){
+		if(count==0){
+			System.out.println(System.currentTimeMillis()-s);
 			return;
 		}
 		ps.forEach((k, v)->{
-			String name = String.valueOf((char)k.intValue());
-			name = name+name+name;
+			String name = persons.get(k);
 			System.out.println(name+" "+ps.get(k));
 		});
+		System.out.println(System.currentTimeMillis()-s);
 	}
 
-	private static void dfs(int i, List<Integer> list) {
+	private static int getKeyFromValue(Map<Integer, String> persons, String name1) {
+		Set<Map.Entry<Integer, String>> set =  persons.entrySet();
+		for (Map.Entry<Integer, String> en : set) {
+			if(en.getValue().equals(name1)) {
+				return en.getKey();
+			}
+		}
+		return 0;
+	}
+
+	private static void dfs(int i, Set<Integer> list) {
 		v[i] = 1;
 		list.add(i);
-//		System.out.println(i+" "+(char)i);
 		for (int j = 0; j < maxpersons; j++) {
 			if(g[i][j] == 0){
 				continue;
 			}
-			list.set(0, list.get(0)+g[i][j]);
 			if(v[j] > 0) {
 				continue;
 			}
